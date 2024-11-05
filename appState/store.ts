ae3801e899a5629402803cb4d12748aea984f383
Patryk Karwowski <p-karwowski-dev@gmail.com>
2024-11-05 10:08:05 +0000

    added setupListeners to store

--- appState/store.ts
+++ appState/store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cmsReducer from './cms/cmsSlice'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store = configureStore({
   reducer: {
     cms: cmsReducer,
   },
 })
+
+setupListeners(store.dispatch)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
